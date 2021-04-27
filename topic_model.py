16689aab34daf1a34d3187c8b98f2e3d6c4e3277
DriesOome <57561283+DriesOome@users.noreply.github.com>
2021-04-27 19:29:48 +0200

    dries branch added

---
+++ topic_model.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 21 14:22:41 2021
+
+@author: dries
+"""
+# This script performs an LDA analysis on a clean set of data
+# importing modules
+import glob
+from gensim.corpora.dictionary import Dictionary
+from gensim.models import LdaMulticore
+from gensim.test.utils import datapath
+from datetime import datetime
+import os
+
+print("start run:")
+startTime = datetime.now().time()
+print(startTime)
+# Reading data 
+files = glob.glob("./clean_data/*")
+data = []
+for i in range(len(files)):
+    file = open(files[i])
+    data.append([])
+    for line in file.readlines():
+        data[i].append(line.rstrip()) # rstrip removes the newline char
+    file.close()
+usedData = []
+
+dictionary = Dictionary(data) 
+corpus = [dictionary.doc2bow(text) for text in data]
+if __name__ == '__main__':
+    lda = LdaMulticore(corpus, id2word=dictionary, num_topics=12, 
+                                        chunksize=5,
+                                        passes=500,
+                                        iterations=5000,
+                                        alpha = 'asymmetric',
+                                        random_state = 100
+                                        )
+    # best model so far
+    # lda = LdaMulticore(corpus, id2word=dictionary, num_topics=15, 
+    #                                     chunksize=5,
+    #                                     passes=500,
+    #                                     iterations=5000,
+    #                                     alpha = 'asymmetric',
+                                        
+    #                                     )
+
+    # saving model to disk
+    saveFolder = os.getcwd()+"/lda_files"
+    try:
+        for root, dirs, files in os.walk(saveFolder):
+            for file in files:
+                try:
+                    os.remove(os.path.join(root, file))
+                except:
+                    print(file+": no such file")
+            os.rmdir(root)
+        os.mkdir(saveFolder)
+    except:
+        print("save folder creation failed")    
+    ldaSave = datapath(saveFolder+"/lda_model")
+    lda.save(ldaSave)
+
+    print("end run:")
+    endTime = startTime = datetime.now().time()
+    print(endTime)
